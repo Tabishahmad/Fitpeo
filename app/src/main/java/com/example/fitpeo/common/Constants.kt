240fe14daffdd9b832bc0991e1817117471e8a7d
Tabishahmad <tabishahmad1@gmail.com>
2023-07-10 19:22:30 +0530

    final one v4

--- app/src/main/java/com/example/fitpeo/common/Constants.kt
+++ app/src/main/java/com/example/fitpeo/common/Constants.kt
@@ -1,5 +1,31 @@
 package com.example.fitpeo.common
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkInfo
+import android.os.Build
+import androidx.annotation.RequiresApi
+
 const val DATABASE_TABLE_NAME = "favourite_book_table"
-const val LIST_INDEX = "listindex"
 const val ALBUM_OBJ = "album_obj"
+
+
+fun isNetworkAvailable(context: Context): Boolean {
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    var isConnected: Boolean
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+       val network = connectivityManager.activeNetwork ?: return false
+       val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+       isConnected = when {
+           activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+           activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+           else -> false
+       }
+    } else {
+        val networkInfo: NetworkInfo? = connectivityManager.activeNetworkInfo
+        isConnected = networkInfo?.isConnectedOrConnecting ?: false
+    }
+
+    return isConnected
+}
--- app/src/main/java/com/example/fitpeo/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/example/fitpeo/presentation/splash/SplashActivity.kt
@@ -1,12 +1,15 @@
 package com.example.fitpeo.presentation.splash
 
 import android.content.Intent
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.fitpeo.BR
 import com.example.fitpeo.R
+import com.example.fitpeo.common.isNetworkAvailable
 import com.example.fitpeo.common.observeOnce
+import com.example.fitpeo.common.showCustomToast
 import com.example.fitpeo.databinding.ActivitySplashBinding
 import com.example.fitpeo.presentation.FragmentContainerActivity
 import com.example.fitpeo.presentation.core.base.BaseActivity
@@ -32,8 +35,13 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_spl
         binding.setVariable(BR.viewModel,mViewModel)
     }
     override fun observeViewModel() {
-        mViewModel.onStartEvent.observeOnce(this) {
-            navigateToGridScreen()
+        //If no condition is required, then I will observe only once.
+        mViewModel.onStartEvent.observe(this) {
+            if (isNetworkAvailable(applicationContext)) {
+                navigateToGridScreen()
+            } else {
+                "No internet connection".showCustomToast(applicationContext, Toast.LENGTH_LONG)
+            }
         }
     }
 }
